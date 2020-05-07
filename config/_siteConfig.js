9f5ea9e0490acded026672a4a68491d63f5e8171
scriptbased <joel@scriptbased.com>
2020-05-07 10:55:52 -0400

    Update _siteConfig.js

--- config/_siteConfig.js
+++ config/_siteConfig.js
@@ -15,7 +15,7 @@ export default {
   logo: 'logo-component', // 'logo-component', // or '/logo.svg' for regular image
   googleAnalytics: {
     on: true,
-    id: UA-165869783-1
+    id: process.env.UA-165869783-1
   },
   mainMenu: [
     {
